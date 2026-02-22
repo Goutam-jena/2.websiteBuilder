856dd74e463e269655f9fd14976994a84215ef5c
Goutam Jena <150872590+Goutam-jena@users.noreply.github.com>
2026-02-22 20:45:40 +0530

    Refactor routing and user authentication in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,55 @@
-import React from 'react'
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom"
-import Home from './pages/Home'
-import useGetCurrentUser from './hooks/useGetCurrentUser'
-import { useSelector } from 'react-redux'
-import Dashboard from './pages/Dashboard'
-import Generate from './pages/Generate'
-import WebsiteEditor from './pages/Editor'
-import LiveSite from './pages/LiveSite'
-import Pricing from './pages/Pricing'
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-export const serverUrl="https://two-websitebuilder-wlj4.onrender.com"
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Generate from "./pages/Generate";
+import WebsiteEditor from "./pages/Editor";
+import LiveSite from "./pages/LiveSite";
+import Pricing from "./pages/Pricing";
+import useGetCurrentUser from "./hooks/useGetCurrentUser";
+
+// ======================
+// Backend URL (Render)
+// ======================
+export const serverUrl = "https://two-websitebuilder-wlj4.onrender.com";
+
 function App() {
-  useGetCurrentUser()
-  const {userData}=useSelector(state=>state.user)
+  // Fetch current user on app load
+  useGetCurrentUser();
+
+  const { userData } = useSelector((state) => state.user);
+
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/dashboard' element={userData?<Dashboard/>:<Home/>}/>
-     <Route path='/generate' element={userData?<Generate/>:<Home/>}/>
-     <Route path='/editor/:id' element={userData?<WebsiteEditor/>:<Home/>}/>
-      <Route path='/site/:id' element={<LiveSite/>}/>
-       <Route path='/pricing' element={<Pricing/>}/>
-   </Routes>
-   </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/site/:id" element={<LiveSite />} />
+        <Route path="/pricing" element={<Pricing />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/dashboard"
+          element={userData ? <Dashboard /> : <Navigate to="/" />}
+        />
+
+        <Route
+          path="/generate"
+          element={userData ? <Generate /> : <Navigate to="/" />}
+        />
+
+        <Route
+          path="/editor/:id"
+          element={userData ? <WebsiteEditor /> : <Navigate to="/" />}
+        />
+
+        {/* Fallback route (optional but good) */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
